e43b531cc9d4e669c97fd9a133934b3a72edf3ea
Daniel <ldaniel.eng@gmail.com>
2023-01-11 15:05:16 -0300

    feat: add google adapter

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import NextAuth from 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    name: string
+    email: string
+    avatar_url: string
+  }
+}
