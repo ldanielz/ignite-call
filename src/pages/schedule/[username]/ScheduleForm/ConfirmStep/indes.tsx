3adad7e97e0868d746d3b02218099184ebf5de9e
Daniel <ldaniel.eng@gmail.com>
2023-01-12 22:24:10 -0300

    feat: add calendar component

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/indes.tsx
@@ -0,0 +1,7 @@
+export default function ConfirmStep() {
+  return (
+    <div>
+      <p>Confirm Step</p>
+    </div>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,7 @@
+import CalendarStep from './CalendarStep'
+
+interface IScheduleFormProps {}
+
+export default function ScheduleForm(props: IScheduleFormProps) {
+  return <CalendarStep />
+}
