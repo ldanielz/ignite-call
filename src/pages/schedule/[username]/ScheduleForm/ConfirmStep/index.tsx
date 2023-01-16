9db6351755c26c5cec618239d1b52758dd62606f
Daniel <ldaniel.eng@gmail.com>
2023-01-16 12:37:17 -0300

    feat: confirmation flow

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Text, TextArea, TextInput } from '@enay-ui/react'
 import { zodResolver } from '@hookform/resolvers/zod'
+import dayjs from 'dayjs'
 import { CalendarBlank, Clock } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -13,9 +14,17 @@ const confirmStepFormSchema = z.object({
   observations: z.string().nullable(),
 })
 
+interface ConfirmStepProps {
+  schedulingDate: Date
+  onCancelConfirmation: () => void
+}
+
 type ConfirmFormData = z.infer<typeof confirmStepFormSchema>
 
-export function ConfirmStep() {
+export function ConfirmStep({
+  schedulingDate,
+  onCancelConfirmation,
+}: ConfirmStepProps) {
   const {
     register,
     handleSubmit,
@@ -26,16 +35,19 @@ export function ConfirmStep() {
 
   function handleConfirmSchedule(data: ConfirmFormData) {}
 
+  const describeDate = dayjs(schedulingDate).format('DD[ de ]MMMM [ de ] YYYY')
+  const describeTime = dayjs(schedulingDate).format('HH:mm[h]')
+
   return (
     <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmSchedule)}>
       <FormHeader>
         <Text>
           <CalendarBlank />
-          22 de Setembro de 2022
+          {describeDate}
         </Text>
         <Text>
           <Clock />
-          18:00h
+          {describeTime}
         </Text>
       </FormHeader>
 
@@ -63,7 +75,7 @@ export function ConfirmStep() {
       </label>
 
       <FormActions>
-        <Button type="button" variant="tertiary">
+        <Button type="button" variant="tertiary" onClick={onCancelConfirmation}>
           Cancelar
         </Button>
         <Button type="submit" disabled={isSubmitting}>
--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
 import { ConfirmStep } from './ConfirmStep'
 
-interface IScheduleFormProps {}
+export default function ScheduleForm() {
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
 
-export default function ScheduleForm(props: IScheduleFormProps) {
-  return <CalendarStep />
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
